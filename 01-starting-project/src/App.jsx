cff2a78fff9e49b37a5cbd93ac1fac54617ca312
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-02-22 16:03:52 +0100

    create Header.jsx component and Header.css

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,26 +1,4 @@
-import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data.js';
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1))
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-  <header>
-  <img src={reactImg} alt="Stylized atom" />
-  <h1>React Essentials</h1>
-  <p>
-    {description} React concepts you will need for almost any app you are
-    going to build!
-  </p>
-</header>
-  );
-}
-
 import Header from "./components/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx"
 
