916dbbdb7dfec8b4c0637f81c674831fc8ffa89e
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-02-22 12:56:43 +0100

    adds randomization and javascript tags in App.jsx

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,10 +1,20 @@
+import reactImg from './assets/react-core-concepts.png'
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
   return (
   <header>
-  <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+  <img src={reactImg} alt="Stylized atom" />
   <h1>React Essentials</h1>
   <p>
-    Fundamental React concepts you will need for almost any app you are
+    {description} React concepts you will need for almost any app you are
     going to build!
   </p>
 </header>
