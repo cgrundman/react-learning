e4f77097587c312ae8b24108f0a1f17283474786
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-02-22 19:01:55 +0100

    update data.js and display new information

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -4,9 +4,10 @@ import { CORE_CONCEPTS } from './data.js';
 import Header from "./components/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx"
 import TabButton from "./components/TabButton.jsx"
+import { EXAMPLES } from './data.js'
 
 function App() { 
-  const [ selectedTopic, setSelectedTopic ] = useState('Please Click a Button');
+  const [ selectedTopic, setSelectedTopic ] = useState('components');
 
   function handleSelect(selectedButton) {
     // selectedButton => 'components', 'jsx', 'props', 'state'
@@ -38,7 +39,13 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
