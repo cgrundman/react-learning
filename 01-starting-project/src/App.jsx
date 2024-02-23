0cd8ba319ca5f228937643b9889a9a503075d860
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-02-23 17:00:35 +0100

    dynamic listing added

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -40,10 +40,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
