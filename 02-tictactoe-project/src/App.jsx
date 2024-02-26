fe54e0dd486b95db504ae5cc9bac37b617bafab7
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-02-26 20:36:29 +0100

    add static player names and symbols

--- 02-tictactoe-project/src/App.jsx
+++ 02-tictactoe-project/src/App.jsx
@@ -3,7 +3,22 @@ function App() {
   
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+          </li>
+          <li>
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+          </li>
+        </ol>
+
+        GAME BOARD
+      </div>
+    </main> 
   )
 }
 
