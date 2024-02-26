c70c192e00f463178eb82f48ee777276402d65de
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-02-26 20:07:30 +0100

    cleaned extra line in Examples.jsx

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -30,7 +30,6 @@ export default function Examples() {
     return (
         <Section title="Examples" id="examples">
             <Tabs 
-                           
                 buttons={
                     <>
                         <TabButton 
