aa0c0c37f67e2e4182ede4f779e25a55ee218590
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-02-29 19:19:15 +0100

    add updates gameboard state

--- 02-tictactoe-project/src/components/GameBoard.jsx
+++ 02-tictactoe-project/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,14 +7,24 @@ const initialGameBoard = [
 ]
 
 export default function GameBoard() {
+    const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+            updatedBoard[rowIndex][colIndex] = 'X'
+            return updatedBoard;
+        });
+    }
+
     return (
         <ol id='game-board'>
-            {initialGameBoard.map((row, rowIndex) => (
+            {gameBoard.map((row, rowIndex) => (
             <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => (
                     <li key={colIndex}>
-                        <button>{playerSymbol}</button>
+                        <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                     </li>
                     ))}
                 </ol>
