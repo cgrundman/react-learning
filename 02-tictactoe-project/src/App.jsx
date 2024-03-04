e1b37c178fee40e23d8ff9dbfa25f3efa9950753
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-03-04 10:14:18 +0100

    add function to rematch button

--- 02-tictactoe-project/src/App.jsx
+++ 02-tictactoe-project/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
 
   for (const turn of gameTurns) {
       const { square, player } = turn;
@@ -70,6 +70,10 @@ function App() {
     });
   }
 
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -85,7 +89,7 @@ function App() {
             isActive={activePlayer === 'O'}
           />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} />}
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
         <GameBoard 
           onSelectSquare={handleSelectSquare}
           board={gameBoard}
--- 02-tictactoe-project/src/components/GameOver.jsx
+++ 02-tictactoe-project/src/components/GameOver.jsx
@@ -1,11 +1,11 @@
-export default function GameOver({ winner }) {
+export default function GameOver({ winner, onRestart }) {
     return (
         <div id='game-over'>
             <h2>Game Over!</h2>
             {winner && <p>{winner} won!</p>}
             {!winner && <p>It's a draw!</p>}
             <p>
-                <button>Rematch!</button>
+                <button onClick={onRestart}>Rematch!</button>
             </p>
         </div>
     )
