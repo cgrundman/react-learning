c0d11c52f2c37d775ca2ca5ab850791851cf456a
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-03-04 09:15:16 +0100

    adds button disable feature to prevent overwriting

--- 02-tictactoe-project/src/components/GameBoard.jsx
+++ 02-tictactoe-project/src/components/GameBoard.jsx
@@ -34,7 +34,12 @@ export default function GameBoard({ onSelectSquare, turns }) {
                 <ol>
                     {row.map((playerSymbol, colIndex) => (
                     <li key={colIndex}>
-                        <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                        <button 
+                            onClick={() => onSelectSquare(rowIndex, colIndex)} 
+                            disabled={playerSymbol !== null}
+                        >
+                            {playerSymbol}
+                        </button>
                     </li>
                     ))}
                 </ol>
