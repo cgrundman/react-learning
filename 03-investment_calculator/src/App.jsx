26506341a5b68b7e285c8a8aa4b8f0659acc2967
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-03-04 15:27:02 +0100

    add UserInput component

--- 03-investment_calculator/src/App.jsx
+++ 03-investment_calculator/src/App.jsx
@@ -1,8 +1,14 @@
 import Header from "./components/Header.jsx"
+import UserInput from "./components/UserInput.jsx"
 
 function App() {
   return (
-    <Header />
+    <main>
+      <Header />
+      <div>
+        <UserInput />
+      </div>
+    </main>
   )
 }
 
---
+++ 03-investment_calculator/src/components/UserInput.jsx
@@ -0,0 +1,16 @@
+export default function UserInput() {
+    return (
+        <div id='user-input'>
+            
+            <label>Initial Investment</label>
+            <input></input>
+            <label>Annual Investment</label>
+            <input></input>
+            <label>Expected Return</label>
+            <input></input>
+            <label>Duration</label>
+            <input></input>
+            
+        </div>
+    )
+}
