45acfdcd80ee487a0f37c4e5dd76e70d050d3375
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-03-05 14:35:06 +0100

    corrects format of input group

--- 03-investment_calculator/src/App.jsx
+++ 03-investment_calculator/src/App.jsx
@@ -1,22 +1,34 @@
+import { useState } from "react";
+
 import Header from "./components/Header.jsx"
 import UserInput from "./components/UserInput.jsx"
 import Results from "./components/Results.jsx"
 
+const INVESTMENTDATA = [
+  15000
+]
+
 function App() {
-  const initialInvestment = 15000;
-  const annualInvestment = 1000;
-  const expectedReturn = 5;
-  const duration = 10;
+  // const [ investmentData, setInvestmentData ] = useState(INVESTMENTDATA);
+
+  // function handleInvestmentData() {
+  //   setInvestmentData(investmentData => {
+  //     return{
+  //       ...investmentData
+  //     };
+  //   });
+  // } 
 
   return (
     <main>
       <Header />
+      
       <UserInput />
       <Results 
-        initialInvestment={initialInvestment}
-        annualInvestment={annualInvestment}
-        expectedReturn={expectedReturn}
-        duration={duration}
+        // initialInvestment={initialInvestment}
+        // annualInvestment={annualInvestment}
+        // expectedReturn={expectedReturn}
+        // duration={duration}
       />
     </main>
   )
--- 03-investment_calculator/src/components/UserInput.jsx
+++ 03-investment_calculator/src/components/UserInput.jsx
@@ -1,14 +1,23 @@
-export default function UserInput() {
+import { useState } from "react"
+
+export default function UserInput({ initialData, onInput }) {
+
     return (
-        <div id='user-input'>
-            <label>Initial Investment</label>
-            <input></input>
-            <label>Annual Investment</label>
-            <input></input>
-            <label>Expected Return</label>
-            <input></input>
-            <label>Duration</label>
-            <input></input>
-        </div>
+        <section id='user-input'>
+            <div className="input-group">
+                <p>
+                    <label>Initial Investment ($)</label>
+                    <input type="number"/>
+                    <label>Annual Investment ($)</label>
+                    <input type="number"/>
+                </p>
+                <p>
+                    <label>Expected Return (%)</label>
+                    <input type="number"/>
+                    <label>Duration (years)</label>
+                    <input type="number"/>
+                </p>
+            </div>
+        </section>
     )
 }
