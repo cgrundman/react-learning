a382fda5fb4072b30ad5a83a31b483976a71d7c9
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-03-08 00:03:57 +0100

    restructure Header.css to header.module.css

--- 04-login_styling/src/components/Header.jsx
+++ 04-login_styling/src/components/Header.jsx
@@ -1,12 +1,12 @@
 import logo from '../assets/logo.png';
-import './Header.css'
+import classes from './Header.module.css'
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className={`${classes.paragraph}`}>A community of artists and art-lovers.</p>
     </header>
   );
 }
