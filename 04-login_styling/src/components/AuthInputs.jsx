6b88f704c73fd3ba5c2931ead1f0e1eb1ea973fc
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-03-07 23:52:06 +0100

    adds conditional class formating within jsx

--- 04-login_styling/src/components/AuthInputs.jsx
+++ 04-login_styling/src/components/AuthInputs.jsx
@@ -24,18 +24,28 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <div className="controls">
         <p>
-          <label>Email</label>
+          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>
+            Email
+          </label>
           <input
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+            style={{
+              backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
+            }}
+            // className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
+          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>
+            Password
+          </label>
           <input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            style={{
+              backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
+            }}
+            // className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
