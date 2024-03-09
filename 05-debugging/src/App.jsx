00122abc3d7a17823c8aa6536f13feecd989e129
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-03-09 15:07:31 +0100

    solve text and number error

--- 05-debugging/src/App.jsx
+++ 05-debugging/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     setUserInput((prevUserInput) => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: newValue,
+        [inputIdentifier]: +newValue, // add '+' to add input as number instead of string
       };
     });
   }
