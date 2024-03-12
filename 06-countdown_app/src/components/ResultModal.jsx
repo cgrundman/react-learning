59cdecf0464e5bc06d8100d20c9d7cbaa1a06768
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-03-12 23:15:22 +0100

    creates internal open for ResultModal

--- 06-countdown_app/src/components/ResultModal.jsx
+++ 06-countdown_app/src/components/ResultModal.jsx
@@ -1,8 +1,18 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+    const dialog = useRef();
+
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal();
+            }
+        };
+    });
+    
     return (
-        <dialog ref={ref} className="result-modal">
+        <dialog ref={dialog} className="result-modal">
             <h2>You {result}!</h2>
             <p>
                 The target time was <strong>{targetTime} seconds.</strong>
--- 06-countdown_app/src/components/TimerChallenge.jsx
+++ 06-countdown_app/src/components/TimerChallenge.jsx
@@ -12,7 +12,7 @@ export default function TimerChallenge({ ref, title, targetTime }) {
     function handleStart() {
         timer.current = setTimeout(() => {
             setTimerExpired(true);
-            dialog.current.showModal();
+            dialog.current.open();
         }, targetTime * 1000);
 
         setTimerStarted(true);
