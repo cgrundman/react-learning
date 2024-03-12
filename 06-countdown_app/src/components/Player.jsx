97afc4baed33a11981149ea8079416371a4cfd51
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-03-12 11:47:21 +0100

    add name input

--- 06-countdown_app/src/components/Player.jsx
+++ 06-countdown_app/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [ enteredPlayerName, setEnteredPlayerName] = useState(null);
+  const [ submitted, setSubmitted ] = useState(false);
+
+  function handleChange(event) {
+    setSubmitted(false);
+    setEnteredPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}!</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
