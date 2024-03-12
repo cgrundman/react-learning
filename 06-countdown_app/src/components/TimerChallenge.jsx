7d44eee28372be36530f9e8414ea111c5b91ba7b
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-03-12 12:40:14 +0100

    add start and stop handles toTimeChallenge.jsx

--- 06-countdown_app/src/components/TimerChallenge.jsx
+++ 06-countdown_app/src/components/TimerChallenge.jsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+    const [ timerStarted, setTimerStarted ] = useState(false);
+    const [ timerExpired, setTimerExpired ] = useState(false);
+
+    function handleStart() {
+        setTimeout(() => {
+            setTimerExpired(true);
+        }, targetTime * 1000);
+
+        setTimerStarted(true);
+    }
+
+    function handleStop() {
+        
+    }
+
     return (
         <section className="challenge">
             <h2>{title}</h2>
+            {timerExpired && <p>You lost!</p>}
             <p className="challenge-time">
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button>
-                    Start Challenge
+                <button onClick={handleStart}>
+                    {timerStarted ? 'Stop' : 'Start'} Challenge
                 </button>
             </p>
-            <p className="">
-                Time is running... / Timer Inactive
+            <p className={timerStarted ? 'active' : undefined}>
+                {timerStarted ? 'Time is running...' : 'Timer Inactive'}
             </p>
         </section>
     )
