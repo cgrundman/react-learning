57c89241a66c8bfc437f7843feebb034877a05cf
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-03-13 00:26:03 +0100

    add score calculation and conditional score display

--- 06-countdown_app/src/components/ResultModal.jsx
+++ 06-countdown_app/src/components/ResultModal.jsx
@@ -8,6 +8,7 @@ const ResultModal = forwardRef(function ResultModal(
 
     const userLost = remainingTime <= 0;
     const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
     useImperativeHandle(ref, () => {
         return {
@@ -20,6 +21,7 @@ const ResultModal = forwardRef(function ResultModal(
     return (
         <dialog ref={dialog} className="result-modal">
             {userLost && <h2>You lose!</h2>}
+            {!userLost && <h2>Your score: {score}</h2>}
             <p>
                 The target time was <strong>{targetTime} seconds.</strong>
             </p>
