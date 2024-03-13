edf60f852efd383675ff5d316573181e19367191
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-03-13 23:40:11 +0100

    create sidebar

--- 07-project_management_app/src/App.jsx
+++ 07-project_management_app/src/App.jsx
@@ -1,8 +1,10 @@
+import Sidebar from "./components/Sidebar.jsx";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8">
+      <Sidebar />
+    </main>
   );
 }
 
