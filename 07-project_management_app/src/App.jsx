91a417a90c96b69db2b01c7499624cc9538630d2
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-03-16 14:16:52 +0100

    add delete project function

--- 07-project_management_app/src/App.jsx
+++ 07-project_management_app/src/App.jsx
@@ -64,9 +64,21 @@ function App() {
     });
   };
 
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        )
+      };
+    });
+  };
+
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
 
-  let projectContent = <ActiveProject project={selectedProject} />
+  let projectContent = <ActiveProject project={selectedProject} onDelete={handleDeleteProject} />
 
   if (projectsState.selectedProjectId === null) {
     projectContent = <NewProjectModal onAdd={handleAddProject} onCancel={handleCancelAddProject} />
