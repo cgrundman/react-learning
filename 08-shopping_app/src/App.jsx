423cfbd9b9aa2f5a849732c9fac8fdba1550ec41
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-03-18 19:47:43 +0100

    remove product component from shop component

--- 08-shopping_app/src/App.jsx
+++ 08-shopping_app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
+import Product from './components/Product.jsx'
 import { DUMMY_PRODUCTS } from './dummy-products.js';
 
 function App() {
@@ -71,7 +72,13 @@ function App() {
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
-      <Shop onAddItemToCart={handleAddItemToCart} />
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} onAddToCart={handleAddItemToCart} />
+          </li>
+        ))}
+      </Shop>
     </>
   );
 }
