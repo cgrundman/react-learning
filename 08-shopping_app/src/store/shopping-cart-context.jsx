dcc6c53d39d86f8f723ee691823d5152466a3823
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-03-20 10:13:42 +0100

    add useReducer hook

--- 08-shopping_app/src/store/shopping-cart-context.jsx
+++ 08-shopping_app/src/store/shopping-cart-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useReducer } from 'react';
 
 import { DUMMY_PRODUCTS } from '../dummy-products.js';
 
@@ -8,7 +8,18 @@ export const CartContext = createContext({
     updateItemQuantity: () => {},
 });
 
+function shoppingCartReducer(state, action) {
+    return state;
+}
+
 export default function CartContextProvider({children}) {
+    const [ shoppingCartState, shoppingCartDispatch ] = useReducer(
+        shoppingCartReducer, 
+        {
+            items: [],
+        }
+    );
+
     const [shoppingCart, setShoppingCart] = useState({
         items: [],
     });
@@ -70,7 +81,7 @@ export default function CartContextProvider({children}) {
     };
 
     const ctxValue = {
-        items: shoppingCart.items,
+        items: shoppingCartState.items,
         addItemToCart: handleAddItemToCart,
         updateItemQuantity: handleUpdateCartItemQuantity
     };
