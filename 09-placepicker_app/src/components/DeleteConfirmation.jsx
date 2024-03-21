43c256b7a4f647e306693e1283ed0cc96c389e5d
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-03-21 17:57:43 +0100

    move progress bar to new component

--- 09-placepicker_app/src/components/DeleteConfirmation.jsx
+++ 09-placepicker_app/src/components/DeleteConfirmation.jsx
@@ -1,22 +1,10 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
+
+import ProgressBar from './ProgressBar.jsx'
 
 const TIMER = 3000;
 
-export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  const [remainingTime, setRemainingTime] = useState(TIMER);
-
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setRemainingTime(prevTime => prevTime - 10);
-    }, 10);
-
-    return () => {
-      clearInterval(interval);
-    }
-  }, []);
-  
-  
+export default function DeleteConfirmation({ onConfirm, onCancel }) {  
   useEffect(() => {
     console.log('TIMER SET')
     const timer = setTimeout(() => {
@@ -41,7 +29,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
-      <progress value={remainingTime} max={TIMER} />
+      <ProgressBar timer={TIMER}/>
     </div>
   );
 }
