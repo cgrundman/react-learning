7d28d864c3f7ea9ac632848526ddc6668a69439d
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-11 16:35:45 +0200

    create ConfigureCounter for similar performance as memo
    memo is retained in Counter.jsx, but is no longer needed

--- 11-counter_app/src/App.jsx
+++ 11-counter_app/src/App.jsx
@@ -2,32 +2,23 @@ import { useState } from 'react';
 
 import Counter from './components/Counter/Counter.jsx';
 import Header from './components/Header.jsx';
+import ConfigureCounter from './components/Counter/ConfigureCounter.jsx';
 import { log } from './log.js';
 
 function App() {
   log('<App /> rendered');
 
-  const [enteredNumber, setEnteredNumber] = useState(0);
   const [chosenCount, setChosenCount] = useState(0);
 
-  function handleChange(event) {
-    setEnteredNumber(+event.target.value);
-  }
-
-  function handleSetClick() {
-    setChosenCount(enteredNumber);
-    setEnteredNumber(0);
+  function handleSetCount(newCount) {
+    setChosenCount(newCount);
   }
 
   return (
     <>
       <Header />
       <main>
-        <section id="configure-counter">
-          <h2>Set Counter</h2>
-          <input type="number" onChange={handleChange} value={enteredNumber} />
-          <button onClick={handleSetClick}>Set</button>
-        </section>
+        <ConfigureCounter onSet={handleSetCount} />
         <Counter initialCount={chosenCount} />
       </main>
     </>
