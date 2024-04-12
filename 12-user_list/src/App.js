945c6631503cff171bee0a2d7d9b973e6eae79ed
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-12 20:04:09 +0200

    add UserFinder component

--- 12-user_list/src/App.js
+++ 12-user_list/src/App.js
@@ -1,9 +1,9 @@
-import Users from './components/Users';
+import UserFinder from './components/UserFinder';
 
 function App() {
   return (
     <div>
-      <Users />
+      <UserFinder />
     </div>
   );
 }
--- 12-user_list/src/components/Users.js
+++ 12-user_list/src/components/Users.js
@@ -3,12 +3,6 @@ import { Component } from 'react';
 import User from './User';
 import classes from './Users.module.css';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
-
 class Users extends Component {
   constructor() {
     super()
@@ -28,7 +22,7 @@ class Users extends Component {
   render() {
     const usersList = (
       <ul>
-        {DUMMY_USERS.map((user) => (
+        {this.props.users.map((user) => (
           <User key={user.id} name={user.name} />
         ))}
       </ul>
