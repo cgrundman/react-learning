9c413ce96e489e2e2f8d4274912e72d12fef76b9
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-16 14:56:09 +0200

    create api call to backend

--- 13-placepicker_database/src/components/AvailablePlaces.jsx
+++ 13-placepicker_database/src/components/AvailablePlaces.jsx
@@ -1,10 +1,24 @@
+import { useState, useEffect } from 'react';
+
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/places')
+      .then((responce) => {
+        return responce.json();
+      })
+      .then((resData) => {
+        setAvailablePlaces(resData.places);
+      });
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
