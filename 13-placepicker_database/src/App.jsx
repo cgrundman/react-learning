cf4d4c421b358d40cc3699cc10e40edae60cdef6
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-29 18:41:57 +0200

    add update user places to https.js

--- 13-placepicker_database/src/App.jsx
+++ 13-placepicker_database/src/App.jsx
@@ -5,6 +5,7 @@ import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
+import { updateUserPlaces } from './http.js';
 
 function App() {
   const selectedPlace = useRef();
@@ -22,7 +23,7 @@ function App() {
     setModalIsOpen(false);
   }
 
-  function handleSelectPlace(selectedPlace) {
+  async function handleSelectPlace(selectedPlace) {
     setUserPlaces((prevPickedPlaces) => {
       if (!prevPickedPlaces) {
         prevPickedPlaces = [];
@@ -32,6 +33,12 @@ function App() {
       }
       return [selectedPlace, ...prevPickedPlaces];
     });
+
+    try {
+      await updateUserPlaces([selectedPlace, ...userPlaces]);
+    } catch (error) {
+      //...
+    }
   }
 
   const handleRemovePlace = useCallback(async function handleRemovePlace() {
--- 13-placepicker_database/src/http.js
+++ 13-placepicker_database/src/http.js
@@ -8,3 +8,21 @@ export async function fetchAvailablePlaces() {
 
     return resData.places;
 }
+
+export async function updateUserPlaces(places) {
+    const response = await fetch('http://localhost:3000/user-places', {
+        method: 'PUT',
+        body: JSON.stringify({ places }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const resData = await response.json();
+
+    if (!response.ok) {
+        throw new Error('Failed to update user data.');
+    }
+
+    return resData.message;
+}
