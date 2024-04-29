858792c8d029d807fa55ae58a79e75a472d3b230
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-29 18:16:30 +0200

    create error message for fetching data

--- 13-placepicker_database/src/components/AvailablePlaces.jsx
+++ 13-placepicker_database/src/components/AvailablePlaces.jsx
@@ -1,22 +1,44 @@
 import { useState, useEffect } from 'react';
 
 import Places from './Places.jsx';
+import Error from './Error.jsx'
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch('http://localhost:3000/places');
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      
+      try {
+        const response = await fetch('http://localhost:3000/place');
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch places');
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({message: error.message || 'Could not fetch places, please try again later.'});
+      }
+
       setIsFetching(false);
     }
 
     fetchPlaces();    
   }, []);
+
+  if (error) {
+    return (
+      <Error 
+        title="An error occured!" 
+        message={error.message} 
+      />
+    )
+  }
 
   return (
     <Places
