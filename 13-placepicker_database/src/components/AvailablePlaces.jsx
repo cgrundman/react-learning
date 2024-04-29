7600d28184c30807e5807d4e9ca7f5bf835b9df9
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-29 18:24:16 +0200

    sort places according to users location

--- 13-placepicker_database/src/components/AvailablePlaces.jsx
+++ 13-placepicker_database/src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 
 import Places from './Places.jsx';
 import Error from './Error.jsx'
+import { sortPlacesByDistance } from '../loc.js'
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -13,18 +14,25 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
       
       try {
-        const response = await fetch('http://localhost:3000/place');
+        const response = await fetch('http://localhost:3000/places');
         const resData = await response.json();
 
         if (!response.ok) {
           throw new Error('Failed to fetch places');
         }
 
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places, 
+            position.coords.latitude, 
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({message: error.message || 'Could not fetch places, please try again later.'});
       }
-
       setIsFetching(false);
     }
 
