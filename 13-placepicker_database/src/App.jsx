0b831ee4171b79a2392bea774f6b33bed2e8d032
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-29 19:14:22 +0200

    add error to Places rendering in App.jsx

--- 13-placepicker_database/src/App.jsx
+++ 13-placepicker_database/src/App.jsx
@@ -1,20 +1,39 @@
-import { useRef, useState, useCallback } from 'react';
+import { useRef, useState, useCallback, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
-import { updateUserPlaces } from './http.js';
+import { fetchUserPlaces, updateUserPlaces } from './http.js';
 import Error from './components/Error.jsx';
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+
   const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        const places = await fetchUserPlaces();
+        setUserPlaces(places);
+      } catch (error) {
+        setError({ message: error.message || 'Failed to fetch user places' });
+
+      }
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+  }, []);
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -96,12 +115,18 @@ function App() {
         </p>
       </header>
       <main>
-        <Places
-          title="I'd like to visit ..."
-          fallbackText="Select the places you would like to visit below."
-          places={userPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
+        {error && <Error title='An error occured!' message={error.message} />}
+
+        {!error && (
+          <Places
+            title="I'd like to visit ..."
+            fallbackText="Select the places you would like to visit below."
+            isLoading={isFetching}
+            loadingText="Fetching your places..."
+            places={userPlaces}
+            onSelectPlace={handleStartRemovePlace}
+          />
+        )}
 
         <AvailablePlaces onSelectPlace={handleSelectPlace} />
       </main>
--- 13-placepicker_database/src/http.js
+++ 13-placepicker_database/src/http.js
@@ -9,6 +9,17 @@ export async function fetchAvailablePlaces() {
     return resData.places;
 }
 
+export async function fetchUserPlaces() {
+    const response = await fetch('http://localhost:3000/user-places');
+    const resData = await response.json();
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch user places');
+    }
+
+    return resData.places;
+}
+
 export async function updateUserPlaces(places) {
     const response = await fetch('http://localhost:3000/user-places', {
         method: 'PUT',
@@ -21,7 +32,7 @@ export async function updateUserPlaces(places) {
     const resData = await response.json();
 
     if (!response.ok) {
-        throw new Error('Failed to update user data.');
+        throw new Error('Failed to update user data');
     }
 
     return resData.message;
