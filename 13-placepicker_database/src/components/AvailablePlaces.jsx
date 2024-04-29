e91cb5bb15b6dace07969cc596b70cda879954a6
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-29 18:27:48 +0200

    move data fetching to its own component

--- 13-placepicker_database/src/components/AvailablePlaces.jsx
+++ 13-placepicker_database/src/components/AvailablePlaces.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 import Error from './Error.jsx'
 import { sortPlacesByDistance } from '../loc.js'
+import { fetchAvailablePlaces } from '../http.js'
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -14,16 +15,11 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
       
       try {
-        const response = await fetch('http://localhost:3000/places');
-        const resData = await response.json();
-
-        if (!response.ok) {
-          throw new Error('Failed to fetch places');
-        }
+        const places = await fetchAvailablePlaces();
 
         navigator.geolocation.getCurrentPosition((position) => {
           const sortedPlaces = sortPlacesByDistance(
-            resData.places, 
+            places, 
             position.coords.latitude, 
             position.coords.longitude
           );
