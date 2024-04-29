e5323aa34f940652038487e254a5b40c09fab301
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-29 19:02:07 +0200

    add error for deleting selected place

--- 13-placepicker_database/src/App.jsx
+++ 13-placepicker_database/src/App.jsx
@@ -51,8 +51,19 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
     );
 
+    try {
+      await updateUserPlaces(
+        userPlaces.filter((place) => place.id !== selectedPlace.current.id)
+      );
+    } catch (error) {
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({
+        message: error.message || 'Failed to delete place.',
+      })
+    }
+
     setModalIsOpen(false);
-  }, []);
+  }, [userPlaces]);
 
   function handleError() {
     setErrorUpdatingPlaces(null);
