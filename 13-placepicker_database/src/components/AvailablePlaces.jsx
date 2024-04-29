35fb0b40552385b50ecf15d916a01d8ba18e9cfd
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-04-29 18:03:31 +0200

    add loading state to display when loading database data

--- 13-placepicker_database/src/components/AvailablePlaces.jsx
+++ 13-placepicker_database/src/components/AvailablePlaces.jsx
@@ -3,22 +3,27 @@ import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/places')
-      .then((responce) => {
-        return responce.json();
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      });
+    async function fetchPlaces() {
+      setIsFetching(true);
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
+    }
+
+    fetchPlaces();    
   }, []);
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
