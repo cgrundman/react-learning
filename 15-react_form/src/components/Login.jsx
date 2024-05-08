c75b6561108cd9c16f5020fd21b33343da16dbc3
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-08 20:48:30 +0200

    add state to handle email and password changes

--- 15-react_form/src/components/Login.jsx
+++ 15-react_form/src/components/Login.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState();
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: ''
+  });
+
   function handleSubmit(event) {
     event.preventDefault();
-    console.log('submitted!');
+
+    console.log(enteredValues)
+    
   }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues(prevValues => ({
+      ...prevValues,
+      [identifier]: value
+    }))
+  }
+
+  // function handleEmailChange(event) {
+  //   setEnteredEmail(event.target.value)
+  // }
+
+  // function handlePasswordChange(event) {
+  //   setEnteredPassword(event.target.value)
+  // }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -11,12 +37,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input 
+            id="email" 
+            type="email" 
+            name="email" 
+            onChange={(event) => handleInputChange('email', event.target.value)}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input 
+            id="password" 
+            type="password" 
+            name="password" 
+            onChange={(event) => handleInputChange('password', event.target.value)}
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
