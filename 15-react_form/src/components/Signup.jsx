c92f471a1281513c21c2477e4b04702a7c85f392
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-08 21:10:23 +0200

    reset form option

--- 15-react_form/src/components/Signup.jsx
+++ 15-react_form/src/components/Signup.jsx
@@ -7,6 +7,8 @@ export default function Signup() {
         const data = Object.fromEntries(fd.entries());
         data.acquisition = acquisitionChannel;
         console.log(data)
+
+        event.target.reset();
     }
 
     return (
