090181a6a3b42ca66bc304b18c06bd778eeec391
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-09 22:25:53 +0200

    validate using useBlur

--- 15-react_form/src/components/StateLogin.jsx
+++ 15-react_form/src/components/StateLogin.jsx
@@ -8,8 +8,13 @@ export default function Login() {
     password: ''
   });
 
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false
+  })
+
   const emailIsInvalid = 
-    enteredValues.email !== '' && !enteredValues.email.includes('@');
+    didEdit.email && !enteredValues.email.includes('@');
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -23,6 +28,17 @@ export default function Login() {
       ...prevValues,
       [identifier]: value
     }))
+    setDidEdit(prevEdit => ({
+      ...prevEdit,
+      [identifier]: false
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit(prevEdit => ({
+      ...prevEdit,
+      [identifier]: true
+    }));
   }
 
   // function handleEmailChange(event) {
@@ -44,6 +60,7 @@ export default function Login() {
             id="email" 
             type="email" 
             name="email" 
+            onBlur={() => handleInputBlur('email')}
             onChange={(event) => handleInputChange('email', event.target.value)}
             value={enteredValues.email}
           />
