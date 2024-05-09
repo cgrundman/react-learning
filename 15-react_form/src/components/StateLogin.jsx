1383110207916c48716894a657b96449937d1d51
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-09 23:16:59 +0200

    add validation from outsourcing to js file

--- 15-react_form/src/components/StateLogin.jsx
+++ 15-react_form/src/components/StateLogin.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Input from "./Input.jsx";
+import { isEmail, isNotEmpty, hasMinLength } from "../util/validation.js";
 
 export default function Login() {
   // const [enteredEmail, setEnteredEmail] = useState();
@@ -14,8 +15,13 @@ export default function Login() {
     password: false
   })
 
-  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
-  const passwordIsInvalid = didEdit.password && enteredValues.password.trim().length < 6;
+  const emailIsInvalid = 
+    didEdit.email && 
+    !isEmail(enteredValues.email) && 
+    !isNotEmpty(enteredValues.email);
+  const passwordIsInvalid = 
+    didEdit.password && 
+    !hasMinLength(enteredValues.password, 6);
 
   function handleSubmit(event) {
     event.preventDefault();
