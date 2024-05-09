70448cb59f311c037a4cc54198f8ff3e34ed0489
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-09 22:36:23 +0200

    demonstrate submission based validation

--- 15-react_form/src/App.jsx
+++ 15-react_form/src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/Header.jsx';
-import Login from './components/StateLogin.jsx';
+import Login from './components/Login.jsx';
 
 function App() {
   return (
--- 15-react_form/src/components/Login.jsx
+++ 15-react_form/src/components/Login.jsx
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
@@ -10,7 +12,14 @@ export default function Login() {
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value
 
-    console.log(enteredEmail, enteredPassword);
+    const emailIsValid = enteredEmail.includes('@')
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(trie);
+      return;
+    }
+
+    setEmailIsInvalid(false);
   }
 
   return (
@@ -26,6 +35,9 @@ export default function Login() {
             name="email" 
             ref={email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
