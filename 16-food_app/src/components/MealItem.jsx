882d5de31fa43881afd93c12173e7d48ed6a67cd
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-11 21:36:44 +0200

    create currency standardization

--- 16-food_app/src/components/MealItem.jsx
+++ 16-food_app/src/components/MealItem.jsx
@@ -1,3 +1,5 @@
+import { currencyFormatter } from "../util/formatting.js"
+
 export default function MealItem({ meal }) {
     return (
         <li className="meal-item">
@@ -5,7 +7,9 @@ export default function MealItem({ meal }) {
                 <img src={'../backend/public/' + meal.image} alt={meal.name} />
                 <div>
                     <h3>{meal.name}</h3>
-                    <p className="meal-item-price" >{meal.price}</p>
+                    <p className="meal-item-price" >
+                        {currencyFormatter.format(meal.price)}
+                    </p>
                     <p className="meal-item-description">{meal.description}</p>
                 </div>
                 <p className="meal-item-actions">
