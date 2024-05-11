9cce4f0d85cb4208c5aa0d4f87d51f12ea31073f
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-11 21:33:03 +0200

    create MealItem component

--- 16-food_app/src/components/Meals.jsx
+++ 16-food_app/src/components/Meals.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import MealItem from "./MealItem.jsx";
 
 export default function Meals() {
     const [loadedMeals, setLoadedMeals] = useState([]);
@@ -21,7 +22,7 @@ export default function Meals() {
     return (
         <ul id="meals">
             {loadedMeals.map((meal) => (
-                <li key={meal.id}>{meal.name}</li>
+                <MealItem key={meal.id} meal={meal} />
             ))}
         </ul>
     )
