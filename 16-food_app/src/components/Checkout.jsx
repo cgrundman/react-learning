3aa1b6fe82ee54fb3e456382f0e286e03f4176f6
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-12 11:48:27 +0200

    create post request via custom hook

--- 16-food_app/src/components/Checkout.jsx
+++ 16-food_app/src/components/Checkout.jsx
@@ -6,10 +6,23 @@ import { currencyFormatter } from '../util/formatting.js';
 import Input from './UI/Input.jsx';
 import Button from './UI/Button.jsx';
 import UserProgressContext from '../store/UserProgressContext.jsx';
+import useHttp from '../hooks/useHttp.js';
+
+const requestConfig = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}
 
 export default function Checkout() {
     const cartCtx = useContext(CartContext);
     const userProgressCtx = useContext(UserProgressContext)
+
+    const {data, isLoading: isSending, error, sendRequest} = useHttp(
+        'http://localhost:3000/orders', 
+        requestConfig
+    );
 
     const cartTotal = cartCtx.items.reduce(
         (totalPrice, item) => totalPrice + item.quantity * item.price,
@@ -26,18 +39,40 @@ export default function Checkout() {
         const fd = new FormData(event.target);
         const customerData = Object.fromEntries(fd.entries());
 
-        fetch('http://localhost:3000/orders', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
+        sendRequest(
+            JSON.stringify({
                 order: {
                     items: cartCtx.items,
                     customer: customerData
                 }
             })
-        });
+        );
+    }
+
+    let actions = (
+        <>
+            <Button type="button" textOnly onClick={handleClose}>Close</Button>
+            <Button>Submit Order</Button>
+        </>
+    );
+
+    if (isSending) {
+        actions = <span>Sending order data...</span>
+    }
+
+    if (data && !error) {
+        return (
+            <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
+                <h2>Success!</h2>
+                <p>Your order was submitted successfully.</p>
+                <p>
+                    We will get back to you with more details via email within the next few minutes.
+                </p>
+                <p className='"modal-actions'>
+                    <Button onClick={handleClose}>Okay</Button>
+                </p>
+            </Modal>
+        )
     }
 
     return (
@@ -55,10 +90,8 @@ export default function Checkout() {
                     <Input label="Postal Code" type="text" id="postal-code" />
                     <Input label="City" type="text" id="city" />
                 </div>
-                <p className='modal-actions'>
-                    <Button type="button" textOnly onClick={handleClose}>Close</Button>
-                    <Button>Submit Order</Button>
-                </p>
+                {error && <Error title="Failed to submit order" message={error} />}
+                <p className='modal-actions'>{actions}</p>
             </form>
         </Modal>
     )
--- 16-food_app/src/hooks/useHttp.js
+++ 16-food_app/src/hooks/useHttp.js
@@ -19,16 +19,19 @@ export default function useHttp(url, config, initialData) {
     const [ isLoading, setIsLoading] = useState(false);
     const [ error, setError ] = useState();
     
-    const sendRequest = useCallback(async function sendRequest() {
-        setIsLoading(true)
-        try {
-            const resData = await sendHttpRequest(url, config);
-            setData(resData)
-        } catch (error) {
-            setError(error.message || 'Something went wrong!');
-        }
-        setIsLoading(false)
-    }, [url, config]);
+    const sendRequest = useCallback(
+        async function sendRequest(data) {
+            setIsLoading(true)
+            try {
+                const resData = await sendHttpRequest(url, {...config, body: data});
+                setData(resData)
+            } catch (error) {
+                setError(error.message || 'Something went wrong!');
+            }
+            setIsLoading(false)
+        }, 
+        [url, config]
+    );
 
     useEffect(() => {
         if (config && (config.method === 'GET' || !config.method) || !config) {
