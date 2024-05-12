dbc2088c549654e6bc044cb04543e48a390c29b0
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-12 11:09:18 +0200

    create error component

--- 16-food_app/src/components/Meals.jsx
+++ 16-food_app/src/components/Meals.jsx
@@ -1,5 +1,6 @@
 import MealItem from "./MealItem.jsx";
 import useHttp from "../hooks/useHttp.js";
+import Error from "./Error.jsx";
 
 const requestConfig = {};
 
@@ -8,11 +9,15 @@ export default function Meals() {
     const {
         data: loadedMeals, 
         isLoading, 
-        error
+        error,
     } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
     if (isLoading) {
-        return <p>Fetching meals...</p>
+        return <p className="center">Fetching meals...</p>
+    }
+
+    if (error) {
+        return <Error title="Failed to fetch meals." message={error}/>
     }
 
     return (
