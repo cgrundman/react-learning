17bd6a34073e88c15f9291149499b82c4344d7c5
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-12 10:23:37 +0200

    create form submission functionality to checkout

--- 16-food_app/src/components/Checkout.jsx
+++ 16-food_app/src/components/Checkout.jsx
@@ -20,15 +20,35 @@ export default function Checkout() {
         userProgressCtx.hideCheckout();
     }
 
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const fd = new FormData(event.target);
+        const customerData = Object.fromEntries(fd.entries());
+
+        fetch('http://localhost:3000/orders', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                order: {
+                    items: cartCtx.items,
+                    customer: customerData
+                }
+            })
+        });
+    }
+
     return (
         <Modal 
             open={userProgressCtx.progress === 'checkout' } 
             onClose={handleClose}
         >
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>Checkout</h2>
                 <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-                <Input label="Full Name" type="text" id="full-name" />
+                <Input label="Full Name" type="text" id="name" />
                 <Input label="E-Mail Address" type="email" id="email" />
                 <Input label="Street" type="text" id="street" />
                 <div className="control-row">
