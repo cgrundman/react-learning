63530165c549a8249b9bf6542af7ffa0c1560068
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-21 15:18:45 +0200

    add toggle to counter display

--- 17-redux/src/components/Counter.js
+++ 17-redux/src/components/Counter.js
@@ -5,6 +5,7 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter)
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' })
@@ -21,12 +22,14 @@ const Counter = () => {
     dispatch({ type: 'decrement' })
   }
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' })
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase by 5</button>
--- 17-redux/src/store/index.js
+++ 17-redux/src/store/index.js
@@ -1,21 +1,33 @@
 import { createStore } from 'redux'
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true }
+
+const counterReducer = (state = initialState, action) => {
     if (action.type === 'increment') {
         return {
-            counter: state.counter + 1
+            counter: state.counter + 1,
+            showCounter: state.showCounter
         }
     }
 
     if (action.type === 'increase') {
         return {
-            counter: state.counter + action.value
+            counter: state.counter + action.value,
+            showCounter: state.showCounter
         }
     }
 
     if (action.type === 'decrement') {
         return {
-            counter: state.counter - 1
+            counter: state.counter - 1,
+            showCounter: state.showCounter
+        }
+    }
+
+    if (action.type === 'toggle') {
+        return {
+            showCounter: !state.showCounter,
+            counter: state.counter
         }
     }
 
