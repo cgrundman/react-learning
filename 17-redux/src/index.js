31c11e7c5c423839f9ef53ac3948f5e76432d646
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-21 14:47:26 +0200

    provide redux store to app

--- 17-redux/src/index.js
+++ 17-redux/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'
 
 import './index.css';
 import App from './App';
+import store from './store/index.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
