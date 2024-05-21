ab3ae5e6667ad4e25b4aa533fd87aecf26b84550
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-21 16:18:44 +0200

    dispatch auth actions into header and auth

--- 17-redux/src/App.js
+++ 17-redux/src/App.js
@@ -1,14 +1,19 @@
 import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
 
 import Counter from './components/Counter';
 import Header from './components/Header';
-import Auth from './components/Auth'
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile'
 
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
       <Counter />
     </Fragment>
   );
--- 17-redux/src/components/Header.js
+++ 17-redux/src/components/Header.js
@@ -1,22 +1,34 @@
+import { useSelector, useDispatch } from 'react-redux';
+
 import classes from './Header.module.css';
+import { authActions } from '../store/index.js';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout())
+  }
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <a href='/'>My Products</a>
+            </li>
+            <li>
+              <a href='/'>My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
