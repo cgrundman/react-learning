6f6b944f1722a493e969980e69c03d4054aaa4c0
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-22 11:26:11 +0200

    add firebase backend to manage cart

--- 18-advanced_redux/src/App.js
+++ 18-advanced_redux/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -7,6 +8,14 @@ import Products from './components/Shop/Products';
 function App() {
 
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart)
+
+  useEffect(() => {
+    fetch('https://adv-redux-a22cc-default-rtdb.firebaseio.com/cart.json', { 
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    });
+  }, [cart]);
 
   return (
     <Layout>
