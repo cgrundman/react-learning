e5189669c7706259992dfb5b4b57dda135fcfcfe
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-22 11:47:14 +0200

    create notification for communication with backend

--- 18-advanced_redux/src/App.js
+++ 18-advanced_redux/src/App.js
@@ -1,27 +1,78 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
+
+let isInitial = true;
 
 function App() {
-
+  const dispatch = useDispatch();
   const showCart = useSelector(state => state.ui.cartIsVisible);
-  const cart = useSelector(state => state.cart)
+  const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
 
   useEffect(() => {
-    fetch('https://adv-redux-a22cc-default-rtdb.firebaseio.com/cart.json', { 
-      method: 'PUT',
-      body: JSON.stringify(cart)
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'sending cart data!'
+        })
+      );
+      const response = await fetch('https://adv-redux-a22cc-default-rtdb.firebaseio.com/cart.json', { 
+        method: 'PUT',
+        body: JSON.stringify(cart)
+      });
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.')
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!'
+        })
+      );
+    }
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error',
+          message: 'Sent cart data successfully!'
+        })
+      );
     });
-  }, [cart]);
+    
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification 
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- 18-advanced_redux/src/store/ui-slice.js
+++ 18-advanced_redux/src/store/ui-slice.js
@@ -2,12 +2,19 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
     name: 'ui',
-    initialState: { cartIsVisible: false },
+    initialState: { cartIsVisible: false, notification: null },
     reducers: {
         toggle(state) {
             state.cartIsVisible = !state.cartIsVisible;
-        }
-    }
+        },
+        showNotification(state, action) {
+            state.notification = { 
+                status: action.payload.status, 
+                title: action.payload.title, 
+                message: action.payload.message 
+            };
+        },
+    },
 });
 
 export const uiActions = uiSlice.actions;
