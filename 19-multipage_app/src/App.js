241dbb0db8ab9a1ed37031b82b5160005353c7c4
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-25 22:10:45 +0200

    add Link from react-router-dom

--- 19-multipage_app/src/App.js
+++ 19-multipage_app/src/App.js
@@ -1,12 +1,21 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
 
 import HomePage from "./pages/Home.js";
 import ProductsPage from "./pages/Producst.js";
 
-const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/products', element: <ProductsPage /> }
-]);
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path='/products' element={<ProductsPage />} />
+  </Route>
+)
+
+// const router = createBrowserRouter([
+//   { path: '/', element: <HomePage /> },
+//   { path: '/products', element: <ProductsPage /> }
+// ]);
+
+const router = createBrowserRouter(routeDefinitions)
 
 function App() {
   return <RouterProvider router={router} />;
--- 19-multipage_app/src/pages/Home.js
+++ 19-multipage_app/src/pages/Home.js
@@ -1,6 +1,14 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
     return (
-        <h1>My Home Page</h1>
+        <>
+            <h1>My Home Page</h1>
+            <p>
+                Go to <Link to='/products' >The List of Products</Link>
+            </p>
+        </>
+        
     )
 }
 
