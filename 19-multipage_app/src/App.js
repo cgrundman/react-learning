6183d8e4ca6b6612c3a4a0c995ff6fa4345a6ed8
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-25 21:56:15 +0200

    add products page

--- 19-multipage_app/src/App.js
+++ 19-multipage_app/src/App.js
@@ -1,9 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./pages/Home.js";
+import ProductsPage from "./pages/Producst.js";
 
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
+  { path: '/products', element: <ProductsPage /> }
 ]);
 
 function App() {
---
+++ 19-multipage_app/src/pages/Producst.js
@@ -0,0 +1,7 @@
+function ProductsPage() {
+    return (
+        <h1>The Products Page</h1>
+    )
+}
+
+export default ProductsPage;
