044657233dee5e37bc13dcdcb2a351c0424bb0f8
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-26 15:56:37 +0200

    navigate programmatically with useNavigate

--- 19-multipage_app/src/pages/Home.js
+++ 19-multipage_app/src/pages/Home.js
@@ -1,11 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
+    const navigate = useNavigate(); 
+
+    function navigationHandler() {
+        navigate('/products')
+    }
+
     return (
         <>
             <h1>My Home Page</h1>
             <p>
                 Go to <Link to='/products' >The List of Products</Link>
+            </p>
+            <p>
+                <button onClick={navigationHandler}>Navigate</button>
             </p>
         </>
         
