90ff8ad3554dce7b5f44fef4f088c6dd9cb2d388
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-26 17:38:25 +0200

    map routes for new components

--- 19-adv_multipage_app/frontend/src/App.js
+++ 19-adv_multipage_app/frontend/src/App.js
@@ -20,17 +20,24 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import EventsPage from "./pages/Events";
+import EventDetailPage from "./pages/EventDetail";
+import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <></>
-  }
+  { path: '/', element: <HomePage />},
+  { path: '/events', element: <EventsPage />},
+  { path: '/events/:eventId', element: <EventDetailPage />},
+  { path: '/events/new', element: <NewEventPage />},
+  { path: '/events/:eventId/edit', element: <EditEventPage />},
 ])
 
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
