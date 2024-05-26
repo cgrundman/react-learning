ffe3680a16c6cadfb65117fd841b42a0e2818722
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-26 16:18:12 +0200

    generate links for dynamic paths

--- 19-multipage_app/src/App.js
+++ 19-multipage_app/src/App.js
@@ -1,9 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./pages/Home.js";
-import ProductsPage from "./pages/Producst.js";
+import ProductsPage from "./pages/Products.js";
 import RootLayout from "./pages/Root.js";
 import ErrorPage from "./pages/Error.js";
+import ProductDetailPage from "./pages/ProductDetail.js";
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -19,7 +20,8 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <HomePage /> },
-      { path: '/products', element: <ProductsPage /> }
+      { path: '/products', element: <ProductsPage /> },
+      { path: '/products/:productId', element: <ProductDetailPage />},
     ],
   }
 ]);
