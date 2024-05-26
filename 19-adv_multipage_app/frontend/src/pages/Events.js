4add5698d6faac0e967e7fbd919e09d538c40ede
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-26 18:01:19 +0200

    add dummy events list

--- 19-adv_multipage_app/frontend/src/pages/Events.js
+++ 19-adv_multipage_app/frontend/src/pages/Events.js
@@ -1,6 +1,23 @@
+import { Link } from "react-router-dom";
+
+const EVENTS = [
+    {id: 'e1', title: 'Event 1'},
+    {id: 'e2', title: 'Event 2'},
+    {id: 'e3', title: 'Event 3'},
+];
+
 function EventsPage() {
     return (
-        <h1>Events Page</h1>
+        <>
+            <h1>Events Page</h1>
+            <ul>
+                {EVENTS.map((event) => (
+                    <li key={event.id}>
+                        <Link to={event.id}>{event.title}</Link>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
 
