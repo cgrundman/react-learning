96248389c6a7c07cf942ec6480c3a1e4052d62ce
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-26 16:38:30 +0200

    Update App.js

--- 19-multipage_app/src/App.js
+++ 19-multipage_app/src/App.js
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: '', element: <HomePage /> },
+      { index: true, element: <HomePage /> },
       { path: 'products', element: <ProductsPage /> },
       { path: 'products/:productId', element: <ProductDetailPage />},
     ],
