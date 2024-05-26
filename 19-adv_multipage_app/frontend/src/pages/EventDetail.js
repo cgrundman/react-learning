ea685042284ad89ddd05bf107d559f82e3e94a3c
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-26 18:06:12 +0200

    output event id on each event id page

--- 19-adv_multipage_app/frontend/src/pages/EventDetail.js
+++ 19-adv_multipage_app/frontend/src/pages/EventDetail.js
@@ -1,7 +1,16 @@
+import { Link, useParams } from "react-router-dom";
+
 function EventDetailPage() {
+    const params = useParams(); 
+
     return (
-        <h1>Event Detail Page
-        </h1>
+        <>
+            <h1>Event Detail Page</h1>
+            <p>{params.eventId}</p>
+            <p>
+                <Link to=".." relative="path">Back</Link>
+            </p>
+        </>
     )
 }
 
