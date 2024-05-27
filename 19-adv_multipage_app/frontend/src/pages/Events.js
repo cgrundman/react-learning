20eea31e2fb3f8c1ce1da4edb8f15c980b85c467
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-27 12:19:57 +0200

    example for how to handle loader data

--- 19-adv_multipage_app/frontend/src/pages/Events.js
+++ 19-adv_multipage_app/frontend/src/pages/Events.js
@@ -3,7 +3,8 @@ import { useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-    const events = useLoaderData();
+    const data = useLoaderData();
+    const events = data.events;
 
     return (
         <>
@@ -18,9 +19,8 @@ export async function loader() {
     const response = await fetch('http://localhost:8080/events');
 
     if (!response.ok) {
-    // ...
+        // ...
     } else {
-        const resData = await response.json();
-        return resData.events;
+        return response;
     }
 }
