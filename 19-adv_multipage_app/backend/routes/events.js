b151ec3ab2597a23d90733b1f7e2b855db9f8bfc
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-27 17:14:02 +0200

    show loading to user through defer()

--- 19-adv_multipage_app/backend/routes/events.js
+++ 19-adv_multipage_app/backend/routes/events.js
@@ -12,7 +12,9 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
   try {
     const events = await getAll();
-    res.json({ events: events });
+    setTimeout(() => {
+      res.json({ events: events });
+    }, 2000)
   } catch (error) {
     next(error);
   }
--- 19-adv_multipage_app/frontend/src/pages/Events.js
+++ 19-adv_multipage_app/frontend/src/pages/Events.js
@@ -1,25 +1,24 @@
-import { useLoaderData, json } from 'react-router-dom';
+import { Suspense } from 'react';
+import { useLoaderData, json, defer, Await } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
 
-function EventsPage() {
-    const data = useLoaderData();
 
-    if (data.isError) {
-        return <p>{data.message}</p>
-    }
-    const events = data.events;
+function EventsPage() {
+    const {events} = useLoaderData();
 
     return (
-        <>
-            <EventsList events={events} />
-        </>
-    );
+        <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+            <Await resolve={events}>
+                {(loadedEvents) => <EventsList events={loadedEvents} />}
+            </Await>
+        </Suspense>
+    )
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
     const response = await fetch('http://localhost:8080/events');
 
     if (!response.ok) {
@@ -34,6 +33,13 @@ export async function loader() {
             }
         )
     } else {
-        return response;
+        const resData = await response.json();
+        return resData.events;
     }
+}
+
+export function loader() {
+    return defer({
+        events: loadEvents()
+    })
 }
