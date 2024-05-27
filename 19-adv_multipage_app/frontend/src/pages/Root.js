a046a92179eb9de06bf73dd5939792c63456610e
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-27 12:11:55 +0200

    create loading indicator

--- 19-adv_multipage_app/frontend/src/pages/Root.js
+++ 19-adv_multipage_app/frontend/src/pages/Root.js
@@ -1,12 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation.js"
 
 function RootLayout() {
+    // const navigation = useNavigation();
+
     return (
         <>
             <MainNavigation />
             <main>
+                {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
                 <Outlet />
             </main>
         </>
