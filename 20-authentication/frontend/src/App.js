1a41dc507b67678d9c1d18f02b928e0b8eab8c06
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-29 18:29:59 +0200

    add routing for login and signup, link authentication to backend

--- 20-authentication/frontend/src/App.js
+++ 20-authentication/frontend/src/App.js
@@ -13,7 +13,7 @@ import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
-import AuthenticationPage from './pages/Authentication';
+import AuthenticationPage, { action as authAction } from './pages/Authentication';
 
 const router = createBrowserRouter([
   {
@@ -63,6 +63,7 @@ const router = createBrowserRouter([
       {
         path: 'auth',
         element: <AuthenticationPage />,
+        action: authAction,
       },
     ],
   },
