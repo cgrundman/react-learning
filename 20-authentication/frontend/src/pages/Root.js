3b8f173bc3e9632a3aac45dc9dedfed17bfb9a4f
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-30 21:44:36 +0200

    add logout to root.js

--- 20-authentication/frontend/src/pages/Root.js
+++ 20-authentication/frontend/src/pages/Root.js
@@ -1,9 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
 
 function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
   // const navigation = useNavigation();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    setTimeout(() => {
+      submit(null, {action: '/logout', method: 'post'})
+    }, 1 * 60 * 60 * 1000);
+  }, [token, submit]);
 
   return (
     <>
