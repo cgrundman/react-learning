322d3743471942e70bf535f5ccb194c5c415bac0
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-30 22:06:59 +0200

    add images

--- 20-authentication/frontend/src/util/auth.js
+++ 20-authentication/frontend/src/util/auth.js
@@ -11,12 +11,16 @@ export function getTokenDuration() {
 export function getAuthToken() {
     const token = localStorage.getItem('token');
 
+    if (!token) {
+        return null;
+    }
+
     const tokenDuration = getTokenDuration();
 
     if (tokenDuration < 0) {
         return 'EXPIRED'
     }
-    
+
     return token;
 }
 
