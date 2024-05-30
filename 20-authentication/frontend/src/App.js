f1306fa6099b8e4117cb855b502e73a389664a86
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-05-30 21:37:44 +0200

    add route authentication

--- 20-authentication/frontend/src/App.js
+++ 20-authentication/frontend/src/App.js
@@ -15,7 +15,7 @@ import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import AuthenticationPage, { action as authAction } from './pages/Authentication';
 import { action as logoutAction } from './pages/Logout.js';
-import { tokenLoader } from './util/auth.js';
+import { checkAuthLoader, tokenLoader } from './util/auth.js';
 
 const router = createBrowserRouter([
   {
@@ -49,6 +49,7 @@ const router = createBrowserRouter([
                 path: 'edit',
                 element: <EditEventPage />,
                 action: manipulateEventAction,
+                loader: checkAuthLoader,
               },
             ],
           },
@@ -56,6 +57,7 @@ const router = createBrowserRouter([
             path: 'new',
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader: checkAuthLoader,
           },
         ],
       },
--- 20-authentication/frontend/src/util/auth.js
+++ 20-authentication/frontend/src/util/auth.js
@@ -1,3 +1,5 @@
+import { redirect } from "react-router-dom";
+
 export function getAuthToken() {
     const token = localStorage.getItem('token');
     return token;
@@ -5,4 +7,14 @@ export function getAuthToken() {
 
 export function tokenLoader() {
     return getAuthToken();
+}
+
+export function checkAuthLoader() {
+    const token = getAuthToken();
+
+    if (!token) {
+        return redirect('/auth');
+    }
+
+    return null;
 }
