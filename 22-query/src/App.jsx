086348e461844da59bc72689f2eb77b8dc7b16aa
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-06 22:00:28 +0200

    setup tanatack query

--- 22-query/src/App.jsx
+++ 22-query/src/App.jsx
@@ -3,6 +3,7 @@ import {
   RouterProvider,
   createBrowserRouter,
 } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
@@ -37,8 +38,14 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  )
 }
 
 export default App;
