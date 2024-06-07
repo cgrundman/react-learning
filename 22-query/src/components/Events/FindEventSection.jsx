8a95dc8b813d62a7c865195017af47199aac84e5
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-07 13:59:36 +0200

    query enable/disable

--- 22-query/src/components/Events/FindEventSection.jsx
+++ 22-query/src/components/Events/FindEventSection.jsx
@@ -8,11 +8,12 @@ import EventItem from './EventItem';
 
 export default function FindEventSection() {
   const searchElement = useRef();
-  const [searchTerm, setSearchTerm ] = useState('');
+  const [searchTerm, setSearchTerm ] = useState();
 
-  const { data, isPending, isError, error } = useQuery({
+  const { data, isLoading, isError, error } = useQuery({
     queryKey: ['events', { search: searchTerm }],
-    queryFn: ({ signal }) => fetchEvents({ signal, searchTerm })
+    queryFn: ({ signal }) => fetchEvents({ signal, searchTerm }),
+    enabled: searchTerm !== undefined
   });
 
   function handleSubmit(event) {
@@ -22,7 +23,7 @@ export default function FindEventSection() {
 
   let content = <p>Please enter a search term to find events.</p>
 
-  if (isPending) {
+  if (isLoading) {
     content = <LoadingIndicator />
   }
 
