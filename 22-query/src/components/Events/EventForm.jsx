64aaa70e48330948a3070244a13dbcf9eb0edfde
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-07 14:35:47 +0200

    fetching selectable images data with mutation

--- 22-query/src/components/Events/EventForm.jsx
+++ 22-query/src/components/Events/EventForm.jsx
@@ -1,9 +1,18 @@
 import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 import ImagePicker from '../ImagePicker.jsx';
+import { fetchSelectableImages } from '../../util/http.js';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EventForm({ inputData, onSubmit, children }) {
   const [selectedImage, setSelectedImage] = useState(inputData?.image);
+
+  const { data, isPending, isError } = useQuery({
+    queryKey: ['events-images'],
+    queryFn: fetchSelectableImages
+  });
 
   function handleSelectImage(image) {
     setSelectedImage(image);
@@ -30,13 +39,22 @@ export default function EventForm({ inputData, onSubmit, children }) {
         />
       </p>
 
-      <div className="control">
-        <ImagePicker
-          images={[]}
-          onSelect={handleSelectImage}
-          selectedImage={selectedImage}
+      {isPending && <p>Loading selectable images...</p>}
+      {isError && (
+        <ErrorBlock 
+          title="Failed to load selectable images"
+          message="Please try again later."
         />
-      </div>
+      )}
+      {data && (
+        <div className="control">
+          <ImagePicker
+            images={data}
+            onSelect={handleSelectImage}
+            selectedImage={selectedImage}
+          />
+        </div>
+      )}
 
       <p className="control">
         <label htmlFor="description">Description</label>
--- 22-query/src/util/http.js
+++ 22-query/src/util/http.js
@@ -40,3 +40,18 @@ export async function fetchEvents({ signal, searchTerm }) {
   
     return event;
   }
+
+  export async function fetchSelectableImages({ signal }) {
+    const response = await fetch(`http://localhost:3000/events/images`, { signal });
+  
+    if (!response.ok) {
+      const error = new Error('An error occurred while fetching the images');
+      error.code = response.status;
+      error.info = await response.json();
+      throw error;
+    }
+  
+    const { images } = await response.json();
+  
+    return images;
+  }
