acf50906de52e8036b98fd0253e8b179283bd504
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-07 14:16:13 +0200

    create new event with useMutation

--- 22-query/src/util/http.js
+++ 22-query/src/util/http.js
@@ -19,3 +19,24 @@ export async function fetchEvents({ signal, searchTerm }) {
 
     return events;
   }
+
+  export async function createNewEvent(eventData) {
+    const response = await fetch(`http://localhost:3000/events`, {
+      method: 'POST',
+      body: JSON.stringify(eventData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  
+    if (!response.ok) {
+      const error = new Error('An error occurred while creating the event');
+      error.code = response.status;
+      error.info = await response.json();
+      throw error;
+    }
+  
+    const { event } = await response.json();
+  
+    return event;
+  }
