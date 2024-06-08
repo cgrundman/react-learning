53026b98772d8159c4b9c7b3fea9f6a86080b594
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-08 13:35:53 +0200

    implement optimistic loading with event editing

--- 22-query/src/components/Events/EditEvent.jsx
+++ 22-query/src/components/Events/EditEvent.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import { useMutation, useQuery } from '@tanstack/react-query';
-import { fetchEvent, updateEvent } from '../../util/http.js';
+import { fetchEvent, queryClient, updateEvent } from '../../util/http.js';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
@@ -17,7 +17,23 @@ export default function EditEvent() {
   });
 
   const { mutate } = useMutation({
-    mutationFn: updateEvent
+    mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event;
+
+      await queryClient.cancelQueries({queryKey: ['events', params.id]})
+      const previousEvent = queryClient.getQueryData(['events', params.id]);
+      
+      queryClient.setQueryData(['events', params.id], newEvent);
+
+      return { previousEvent }
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(['events', params.id], context.previousEvent);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(['events', params.id]);
+    }
   }); 
 
   function handleSubmit(formData) {
