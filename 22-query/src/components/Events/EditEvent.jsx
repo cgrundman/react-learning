6855372b08a3f722f460f93822a0b290120b29da
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-08 13:10:48 +0200

    create modal content for updating an event

--- 22-query/src/components/Events/EditEvent.jsx
+++ 22-query/src/components/Events/EditEvent.jsx
@@ -1,10 +1,20 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { useQuery } from '@tanstack/react-query';
+import { fetchEvent } from '../../util/http.js';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const params = useParams();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id })
+  });
 
   function handleSubmit(formData) {}
 
@@ -12,9 +22,35 @@ export default function EditEvent() {
     navigate('../');
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
+  let content;
+
+  if (isPending) {
+    content = (
+      <div className='center'>
+        <LoadingIndicator />
+      </div>
+    )
+  }
+
+  if (isError) {
+    content = (
+      <>
+        <ErrorBlock 
+          title="Failed to load event"
+          message={error.info?.message || 'Failed to load event'}
+        />
+        <div className='form-actions'>
+          <Link to='../' className> 
+            Okay
+          </Link>
+        </div>
+      </>
+    )
+  }
+
+  if (data) {
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
         <Link to="../" className="button-text">
           Cancel
         </Link>
@@ -22,6 +58,10 @@ export default function EditEvent() {
           Update
         </button>
       </EventForm>
-    </Modal>
+    )
+  }
+
+  return (
+    <Modal onClose={handleClose}>{content}</Modal>
   );
 }
