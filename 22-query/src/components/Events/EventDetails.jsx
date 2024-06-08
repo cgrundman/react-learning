df03b167616aa22c77914b84590803f615435ef9
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-08 11:25:28 +0200

    prevent delete event errors with refetchType

--- 22-query/src/components/Events/EventDetails.jsx
+++ 22-query/src/components/Events/EventDetails.jsx
@@ -19,7 +19,8 @@ export default function EventDetails() {
     mutationFn: deleteEvent,
     onSuccess: () => {
       queryClient.invalidateQueries({
-        queryKey: ['events']
+        queryKey: ['events'],
+        refetchType: 'none'
       });
       navigate('/events');
     }
