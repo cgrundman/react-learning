dac2a0e2349ceb28f6364bf95e141a391992bef3
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-08 13:16:25 +0200

    create edit event functionality

--- 22-query/src/components/Events/EditEvent.jsx
+++ 22-query/src/components/Events/EditEvent.jsx
@@ -2,8 +2,8 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
-import { useQuery } from '@tanstack/react-query';
-import { fetchEvent } from '../../util/http.js';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { fetchEvent, updateEvent } from '../../util/http.js';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
@@ -16,7 +16,14 @@ export default function EditEvent() {
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id })
   });
 
-  function handleSubmit(formData) {}
+  const { mutate } = useMutation({
+    mutationFn: updateEvent
+  }); 
+
+  function handleSubmit(formData) {
+    mutate({id: params.id, event: formData});
+    navigate('../');
+  }
 
   function handleClose() {
     navigate('../');
