f7ac1a39830860b918af2565793a8123a8c4516a
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-14 11:51:34 +0200

    add Image component from nextjs

--- 23-nextjs/nextjs_food/components/main-header.js
+++ 23-nextjs/nextjs_food/components/main-header.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
@@ -7,7 +8,7 @@ export default function MainHeader() {
     return (
         <header className={classes.header} >
             <Link className={classes.logo} href="/">
-                <img src={logoImg.src} alt="A plate with food on it" />            
+                <Image src={logoImg} alt="A plate with food on it" priority />            
                 NextLevel Food
             </Link>
 
