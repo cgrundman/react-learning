3ad79c250e1745d51b6220431798fcfeee983051
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-14 12:13:31 +0200

    add files for image slideshow to homepage

--- 23-nextjs/nextjs_food/app/page.js
+++ 23-nextjs/nextjs_food/app/page.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
 
 import classes from './page.module.css';
+import ImageSlideshow from "@/components/main-header/images/image-slideshow";
 
 export default function Home() {
   return (
     <>
       <header className={classes.header}>
-        <div className={classes.slideshow}></div>
+        <div className={classes.slideshow}><ImageSlideshow /></div>
         <div>
           <div className={classes.hero}>
             <h1>NextLevel Food for NextLevel Foodies</h1>
