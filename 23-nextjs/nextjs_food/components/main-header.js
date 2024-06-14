7125fcb55b36d8876b5e4aeff38892eb9924028f
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-14 11:47:12 +0200

    add css styling to header

--- 23-nextjs/nextjs_food/components/main-header.js
+++ 23-nextjs/nextjs_food/components/main-header.js
@@ -1,16 +1,17 @@
 import Link from "next/link";
 
-import logoImg from '@/assets/logo.png'
+import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
 
 export default function MainHeader() {
     return (
-        <header>
-            <Link href="/">
+        <header className={classes.header} >
+            <Link className={classes.logo} href="/">
                 <img src={logoImg.src} alt="A plate with food on it" />            
                 NextLevel Food
             </Link>
 
-            <nav>
+            <nav className={classes.nav}>
                 <ul>
                     <li>
                         <Link href="/meals">Browse Meals</Link>
