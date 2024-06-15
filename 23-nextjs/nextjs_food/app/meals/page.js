a23aa8e6fb8ad406167d7589bb5e2e02d15fb911
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-15 10:37:00 +0200

    add meals from batabase

--- 23-nextjs/nextjs_food/app/meals/page.js
+++ 23-nextjs/nextjs_food/app/meals/page.js
@@ -1,8 +1,11 @@
 import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
 
-export default function MealsPage() {
+export default async function MealsPage() {
+    const meals = await getMeals();
+
     return (
         <>
             <header className={classes.header}>
@@ -18,7 +21,7 @@ export default function MealsPage() {
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={[]} />
+                <MealsGrid meals={meals} />
             </main>
         </>
     )
