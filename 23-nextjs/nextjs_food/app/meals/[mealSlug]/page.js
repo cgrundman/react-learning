3d92311b45171fee5d15f1d4bf70123a38a7ba10
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-15 12:05:10 +0200

    add meal not found page

--- 23-nextjs/nextjs_food/app/meals/[mealSlug]/page.js
+++ 23-nextjs/nextjs_food/app/meals/[mealSlug]/page.js
@@ -2,9 +2,14 @@ import Image from 'next/image'
 
 import classes from './page.module.css'
 import { getMeal } from '@/lib/meals'
+import { notFound } from 'next/navigation';
 
 export default function MealDetailsPage({ params }) {
     const meal = getMeal(params.mealSlug);
+
+    if (!meal) {
+        notFound();
+    }
 
     meal.instructions = meal.instructions.replace(/\n/g, '<br />')
 
