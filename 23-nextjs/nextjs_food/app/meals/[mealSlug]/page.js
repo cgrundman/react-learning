137c807ad0d3111367c5fe392c1922e84616d74f
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-15 11:52:55 +0200

    load and display meal details

--- 23-nextjs/nextjs_food/app/meals/[mealSlug]/page.js
+++ 23-nextjs/nextjs_food/app/meals/[mealSlug]/page.js
@@ -1,7 +1,36 @@
+import Image from 'next/image'
+
+import classes from './page.module.css'
+import { getMeal } from '@/lib/meals'
+
 export default function MealDetailsPage({ params }) {
+    const meal = getMeal(params.mealSlug);
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+
     return (
-        <main>
-            <h1>Meal Details</h1>
-        </main>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${'EMAIL'}`} >{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p 
+                    className={classes.instructions} 
+                    dangerouslySetInnerHTML={{
+                        __html: meal.instructions,
+                    }}
+                >
+                </p>
+            </main>
+        </>
     )
 }
--- 23-nextjs/nextjs_food/lib/meals.js
+++ 23-nextjs/nextjs_food/lib/meals.js
@@ -8,3 +8,7 @@ export async function getMeals() {
     // throw new Error('Loading meals failed')
     return db.prepare('SELECT * FROM meals').all();
 }
+
+export function getMeal(slug) {
+    return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+}
