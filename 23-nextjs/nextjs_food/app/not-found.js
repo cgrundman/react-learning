c1308899f6d310dbd591921e437bf4d710a9281f
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-15 11:11:38 +0200

    Create not-found.js

---
+++ 23-nextjs/nextjs_food/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <h1>Not found</h1>
+            <p>Unfortunately, we could not find that requested page or resource.</p>
+        </main>
+    )
+}
