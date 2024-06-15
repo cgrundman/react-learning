5976912fb92fae654e6e2b46986b83736eaa9fd6
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-15 12:49:41 +0200

    create image picker

--- 23-nextjs/nextjs_food/app/meals/share/page.js
+++ 23-nextjs/nextjs_food/app/meals/share/page.js
@@ -1,3 +1,4 @@
+import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
@@ -38,7 +39,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          IMAGE PICKER
+          <ImagePicker />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
