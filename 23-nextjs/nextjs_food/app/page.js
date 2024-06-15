84fe9041009bab7bdaabdbf70d3ab5c7475baff7
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-15 10:04:55 +0200

    create meals page formatting andstyling

--- 23-nextjs/nextjs_food/app/page.js
+++ 23-nextjs/nextjs_food/app/page.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 import classes from './page.module.css';
-import ImageSlideshow from "@/components/main-header/images/image-slideshow";
+import ImageSlideshow from "@/components/images/image-slideshow";
 
 export default function Home() {
   return (
