95c07aefb987d5fde1ec94ac6f2d8cd670eac288
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-15 09:42:12 +0200

    add community page content, style header with navlink

--- 23-nextjs/nextjs_food/components/main-header/main-header.js
+++ 23-nextjs/nextjs_food/components/main-header/main-header.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
     return (
@@ -18,10 +19,10 @@ export default function MainHeader() {
                 <nav className={classes.nav}>
                     <ul>
                         <li>
-                            <Link href="/meals">Browse Meals</Link>
+                            <NavLink href="/meals">Browse Meals</NavLink>
                         </li>
                         <li>
-                            <Link href="/community">Foodies Community</Link>
+                            <NavLink href="/community">Foodies Community</NavLink>
                         </li>
                     </ul>
                 </nav>
