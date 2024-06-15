cd38f51b8cf5fd95858d4724529bee57d6962367
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-15 10:53:03 +0200

    add header to loading page

--- 23-nextjs/nextjs_food/app/meals/page.js
+++ 23-nextjs/nextjs_food/app/meals/page.js
@@ -1,11 +1,16 @@
+import { Suspense } from 'react';
 import Link from 'next/link';
+
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 
-export default async function MealsPage() {
+async function Meals() {
     const meals = await getMeals();
+    return <MealsGrid meals={meals} />
+}
 
+export default function MealsPage() {
     return (
         <>
             <header className={classes.header}>
@@ -21,7 +26,9 @@ export default async function MealsPage() {
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={meals} />
+                <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+                    <Meals />
+                </Suspense>
             </main>
         </>
     )
