900b0ba929a1079fa32f7f5216c7c02dfde832d8
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-22 18:00:54 +0200

    create meal handler for added meal with slugify and xss

--- 23-nextjs/nextjs_food/lib/meals.js
+++ 23-nextjs/nextjs_food/lib/meals.js
@@ -1,4 +1,6 @@
 import sql from 'better-sqlite3';
+import slugify from 'slugify';
+import xss from 'xss';
 
 const db = sql('meals.db')
 
@@ -11,4 +13,8 @@ export async function getMeals() {
 
 export function getMeal(slug) {
     return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+}
+export function saveMeal(meal) {
+    meal.slug = slugify(meal.title, { lower: true });
+    meal.instructions = xss(meal.instructions);
 }
