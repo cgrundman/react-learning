03c19fc12d7b59b0709be7ecfaa37f5bf530bcba
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-22 17:21:01 +0200

    add server function shareMeal to handle form submission data

--- 23-nextjs/nextjs_food/app/meals/share/page.js
+++ 23-nextjs/nextjs_food/app/meals/share/page.js
@@ -2,6 +2,21 @@ import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
+  async function shareMeal(formData) {
+    'use server';
+
+    const meal = {
+      title: formData.get('title'),
+      summary: formData.get('summary'),
+      instructions: formData.get('instructions'),
+      image: formData.get('image'),
+      creator: formData.get('name'),
+      creator_email: formData.get('email')
+    }
+
+    console.log(meal)
+  }
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +26,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +54,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
