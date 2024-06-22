3ca9f6848a47336b054bc65735eb23d489ac8cb0
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-22 19:19:33 +0200

    add validation error message

--- 23-nextjs/nextjs_food/app/meals/share/page.js
+++ 23-nextjs/nextjs_food/app/meals/share/page.js
@@ -1,3 +1,7 @@
+'use client';
+
+import { useFormState } from 'react-dom';
+
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
--- 23-nextjs/nextjs_food/lib/actions.js
+++ 23-nextjs/nextjs_food/lib/actions.js
@@ -3,7 +3,11 @@
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
-export async function shareMeal(formData) {
+function isInvalidText(text) {
+    return !text || text.trim() === '';
+}
+
+export async function shareMeal(prevState, formData) {
     const meal = {
       title: formData.get('title'),
       summary: formData.get('summary'),
@@ -11,6 +15,21 @@ export async function shareMeal(formData) {
       image: formData.get('image'),
       creator: formData.get('name'),
       creator_email: formData.get('email')
+    }
+
+    if (
+        isInvalidText(meal.title) || 
+            isInvalidText(meal.title) || 
+            isInvalidText(meal.summary) || 
+            isInvalidText(meal.instructions) || 
+            isInvalidText(meal.creator) ||
+            isInvalidText(meal.creator_email) ||
+            !meal.creator_email.includes('@') ||
+            !meal.image || meal.image.size === 0
+    ) {
+        return {
+            message: 'Invalid input.'
+        }
     }
 
     await saveMeal(meal);
