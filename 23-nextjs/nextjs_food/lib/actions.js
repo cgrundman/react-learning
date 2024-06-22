549f1da33bf7d978bdfc5e1fd55e0bdc8f01156e
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-22 18:50:36 +0200

    finish share meal functionality

--- 23-nextjs/nextjs_food/lib/actions.js
+++ 23-nextjs/nextjs_food/lib/actions.js
@@ -1,5 +1,8 @@
 'use server';
 
+import { redirect } from "next/navigation";
+import { saveMeal } from "./meals";
+
 export async function shareMeal(formData) {
     const meal = {
       title: formData.get('title'),
@@ -10,5 +13,6 @@ export async function shareMeal(formData) {
       creator_email: formData.get('email')
     }
 
-    console.log(meal)
+    await saveMeal(meal);
+    redirect('/meals')
   }
--- 23-nextjs/nextjs_food/lib/meals.js
+++ 23-nextjs/nextjs_food/lib/meals.js
@@ -1,3 +1,5 @@
+import fs from 'node:fs';
+
 import sql from 'better-sqlite3';
 import slugify from 'slugify';
 import xss from 'xss';
@@ -14,7 +16,36 @@ export async function getMeals() {
 export function getMeal(slug) {
     return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
 }
-export function saveMeal(meal) {
+export async function saveMeal(meal) {
     meal.slug = slugify(meal.title, { lower: true });
     meal.instructions = xss(meal.instructions);
+
+    const extension = meal.image.name.split('.').pop();
+    const fileName = `${meal.slug}.${extension}`
+
+    const stream = fs.createWriteStream(`public/images/${fileName}`)
+    const bufferedImage = await meal.image.arrayBuffer();
+
+    stream.write(Buffer.from(bufferedImage), (error) =>{
+        if (error) {
+            throw new Error('Saving image failed!');
+        }
+
+    });
+
+    meal.image = `/images/${fileName}`
+
+    db.prepare(`
+        INSERT INTO meals
+            (title, summary, instructions, creator, creator_email, image, slug)
+        VALUES (
+            @title,
+            @summary,
+            @instructions,
+            @creator,
+            @creator_email,
+            @image,
+            @slug
+        )
+    `).run(meal)
 }
