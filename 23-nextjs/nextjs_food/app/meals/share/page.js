b3a8bbd04479bb7f71005b19ccb47e8be41e8cb6
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-22 18:58:39 +0200

    conditionally render button for sharing meal

--- 23-nextjs/nextjs_food/app/meals/share/page.js
+++ 23-nextjs/nextjs_food/app/meals/share/page.js
@@ -1,10 +1,9 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
-  
-
   return (
     <>
       <header className={classes.header}>
@@ -44,7 +43,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
---
+++ 23-nextjs/nextjs_food/components/meals/meals-form-submit.js
@@ -0,0 +1,14 @@
+'use client';
+
+import { useFormStatus } from 'react-dom';
+
+
+export default function MealsFormSubmit() {
+    const { pending } = useFormStatus();
+
+    return (
+        <button disabled={pending}>
+            {pending ? 'Submitting...' : 'Share Meal'}
+        </button>
+    )
+}
