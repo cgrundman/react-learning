b4485381dada0abf884fe9b5667e7951269717c7
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-22 17:23:40 +0200

    move server function into its own file

--- 23-nextjs/nextjs_food/app/meals/share/page.js
+++ 23-nextjs/nextjs_food/app/meals/share/page.js
@@ -1,21 +1,9 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/actions';
 
 export default function ShareMealPage() {
-  async function shareMeal(formData) {
-    'use server';
-
-    const meal = {
-      title: formData.get('title'),
-      summary: formData.get('summary'),
-      instructions: formData.get('instructions'),
-      image: formData.get('image'),
-      creator: formData.get('name'),
-      creator_email: formData.get('email')
-    }
-
-    console.log(meal)
-  }
+  
 
   return (
     <>
