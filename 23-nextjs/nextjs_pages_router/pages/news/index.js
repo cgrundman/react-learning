71b7bd460158d5f4ef68fcaab91b3434ed5f5fe9
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-24 13:56:31 +0200

    add Link component from nextjs

--- 23-nextjs/nextjs_pages_router/pages/news/index.js
+++ 23-nextjs/nextjs_pages_router/pages/news/index.js
@@ -1,8 +1,25 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 // our-domain.com/news
 
 function NewsPage() {
     return (
-        <h1>The News Page</h1>
+        <Fragment>
+            <h1>The News Page</h1>
+            <ul>
+                <li>
+                    <Link href="/news/nextjs-is-a-great-framework">
+                        NextJS Is a Great Framework
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/news/something-else">
+                        Something Else
+                    </Link>
+                </li>
+            </ul>
+        </Fragment>
     )
 }
 
