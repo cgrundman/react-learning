8d384cfbc34461bdeafd5ea235c798ecaf822627
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-24 13:04:06 +0200

    create static metadata

--- 23-nextjs/nextjs_food/app/meals/page.js
+++ 23-nextjs/nextjs_food/app/meals/page.js
@@ -5,6 +5,11 @@ import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 
+export const metadata = {
+    title: 'All Meals',
+    description: 'Browse the delicious meals shared by our vibrant community.',
+  };
+
 async function Meals() {
     const meals = await getMeals();
     return <MealsGrid meals={meals} />
