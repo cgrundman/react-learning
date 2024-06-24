3dfedca06846d365dd847ba5566d339aad119d79
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-24 13:48:54 +0200

    dyanmicly change page content for dynamic pages

--- 23-nextjs/nextjs_pages_router/pages/news/[newsid].js
+++ 23-nextjs/nextjs_pages_router/pages/news/[newsid].js
@@ -1,8 +1,16 @@
+import { useRouter } from "next/router";
+
 // our-domain.com/news/something-important
 
 function DetailPage() {
+    const router = useRouter();
+
+    const newId = router.query.newsid;
+
+    // send request to backend API to fetch news item
+
     return (
-        <h1>The Detail Page</h1>
+        <h1>The {newId} Page</h1>
     )
 }
 
