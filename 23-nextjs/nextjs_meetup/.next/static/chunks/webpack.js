1d4ba498c9c83cf9ab05059a3f3703654d565b1b
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-25 14:36:08 +0200

    add Layout to all pages

--- 23-nextjs/nextjs_meetup/.next/static/chunks/webpack.js
+++ 23-nextjs/nextjs_meetup/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4e954830a39fd070"; }
+/******/ 		__webpack_require__.h = function() { return "cfc515159bb3b408"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
