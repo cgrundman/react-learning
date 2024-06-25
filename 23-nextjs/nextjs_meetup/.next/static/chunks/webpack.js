96c631cb763bc844573908e1c8fba1a1e17c72b8
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-25 14:27:26 +0200

    add new meetup form

--- 23-nextjs/nextjs_meetup/.next/static/chunks/webpack.js
+++ 23-nextjs/nextjs_meetup/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "63972e072de38412"; }
+/******/ 		__webpack_require__.h = function() { return "4e954830a39fd070"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- 23-nextjs/nextjs_meetup/pages/new-meetup/index.js
+++ 23-nextjs/nextjs_meetup/pages/new-meetup/index.js
@@ -1,1 +1,13 @@
-// our-domain.com/new-meetup
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+function NewMeetupPage() {
+    function addMeetupHandler(enteredMeetupData) {
+        console.log(enteredMeetupData);
+    }
+
+    return (
+        <NewMeetupForm onAddMeetup = {addMeetupHandler} />
+    )
+}
+
+export default NewMeetupPage
