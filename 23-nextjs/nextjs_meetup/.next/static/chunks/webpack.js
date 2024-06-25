4d05e7f21f0834461ec01ac9763edf0bb747e6a0
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-25 14:22:18 +0200

    create homepage

--- 23-nextjs/nextjs_meetup/.next/static/chunks/webpack.js
+++ 23-nextjs/nextjs_meetup/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ae441906cebf0bad"; }
+/******/ 		__webpack_require__.h = function() { return "63972e072de38412"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- 23-nextjs/nextjs_meetup/.next/static/webpack/webpack.b087be276369f53b.hot-update.js
+++ 23-nextjs/nextjs_meetup/.next/static/webpack/webpack.b087be276369f53b.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "ae441906cebf0bad"; }
+/******/ 	__webpack_require__.h = function() { return "d4b18e7a97636a53"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
