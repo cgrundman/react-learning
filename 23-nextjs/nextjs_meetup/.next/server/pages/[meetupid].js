76603316a585620816b50d854a0fb008b3148eaf
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-25 14:40:44 +0200

    add dynamic routing for meetup detail page

---
+++ 23-nextjs/nextjs_meetup/.next/server/pages/[meetupid].js
@@ -0,0 +1,35 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/[meetupid]";
+exports.ids = ["pages/[meetupid]"];
+exports.modules = {
+
+/***/ "./pages/[meetupid]/index.js":
+/*!***********************************!*\
+  !*** ./pages/[meetupid]/index.js ***!
+  \***********************************/
+/***/ (() => {
+
+eval("// our-domain.com/[meetupId]\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9uZXh0anMtY291cnNlLy4vcGFnZXMvW21lZXR1cGlkXS9pbmRleC5qcz9jNzdmIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIG91ci1kb21haW4uY29tL1ttZWV0dXBJZF0iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsNEJBQTRCIiwiZmlsZSI6Ii4vcGFnZXMvW21lZXR1cGlkXS9pbmRleC5qcy5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/[meetupid]/index.js\n");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/[meetupid]/index.js"));
+module.exports = __webpack_exports__;
+
+})();
--- 23-nextjs/nextjs_meetup/.next/static/chunks/webpack.js
+++ 23-nextjs/nextjs_meetup/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cfc515159bb3b408"; }
+/******/ 		__webpack_require__.h = function() { return "088ebeec2921db0d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
