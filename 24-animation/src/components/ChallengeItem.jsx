463da832030bd9cd4a86ccb70645a33920dc521c
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-06-30 13:14:52 +0200

    craete animation with css transitions

--- 24-animation/src/components/ChallengeItem.jsx
+++ 24-animation/src/components/ChallengeItem.jsx
@@ -42,7 +42,7 @@ export default function ChallengeItem({
             </p>
           </div>
         </header>
-        <div className="challenge-item-details">
+        <div className={`challenge-item-details ${isExpanded ? 'expanded' : ''}`}>
           <p>
             <button onClick={onViewDetails}>
               View Details{' '}
