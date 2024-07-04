86c2a516b2c9b62c7091e5f194de90ca51521409
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-07-04 13:34:59 +0200

    animating modal with multiple states

--- 24-animation/src/components/Modal.jsx
+++ 24-animation/src/components/Modal.jsx
@@ -6,9 +6,13 @@ export default function Modal({ title, children, onClose }) {
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog 
-        initial={{ opacity: 0, y: 30 }}
-        animate={{ opacity: 1, y: 0}}
-        exit={{ opacity: 0, y: 30 }}
+        variants={{
+          hidden: { opacity: 0, y: 30 },
+          visible: { opacity: 1, y: 0}
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
         open 
         className="modal"
       >
