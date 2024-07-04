8276f49639070e7ffd57027f56d92f6c61416108
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-07-04 14:44:35 +0200

    add imperative animation
    bug, also doesnt work

--- 24-animation/src/components/NewChallenge.jsx
+++ 24-animation/src/components/NewChallenge.jsx
@@ -1,5 +1,5 @@
 import { useContext, useRef, useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, useAnimate } from 'framer-motion';
 
 import { ChallengesContext } from '../store/challenges-context.jsx';
 import Modal from './Modal.jsx';
@@ -9,6 +9,8 @@ export default function NewChallenge({ onDone }) {
   const title = useRef();
   const description = useRef();
   const deadline = useRef();
+
+  const [scope, animate] = useAnimate();
 
   const [selectedImage, setSelectedImage] = useState(null);
   const { addChallenge } = useContext(ChallengesContext);
@@ -32,6 +34,11 @@ export default function NewChallenge({ onDone }) {
       !challenge.deadline.trim() ||
       !challenge.image
     ) {
+      animate(
+        'input, textarea', 
+        { x: [-10, 0, 10, 0] }, 
+        { type: 'spring', duration: 0.2, delay: stagger(0.05) }
+      );
       return;
     }
 
@@ -41,7 +48,7 @@ export default function NewChallenge({ onDone }) {
 
   return (
     <Modal title="New Challenge" onClose={onDone}>
-      <form id="new-challenge" onSubmit={handleSubmit}>
+      <form id="new-challenge" onSubmit={handleSubmit} ref={scope}>
         <p>
           <label htmlFor="title">Title</label>
           <input ref={title} type="text" name="title" id="title" />
