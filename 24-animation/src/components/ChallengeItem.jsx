5a641bffef5b27c54ac96772e570f6c93c39ad75
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-07-04 14:54:22 +0200

    animate layout

--- 24-animation/src/components/ChallengeItem.jsx
+++ 24-animation/src/components/ChallengeItem.jsx
@@ -28,7 +28,7 @@ export default function ChallengeItem({
   }
 
   return (
-    <li>
+    <motion.li layout>
       <article className="challenge-item">
         <header>
           <img {...challenge.image} />
@@ -62,6 +62,6 @@ export default function ChallengeItem({
           )}
         </div>
       </article>
-    </li>
+    </motion.li>
   );
 }
