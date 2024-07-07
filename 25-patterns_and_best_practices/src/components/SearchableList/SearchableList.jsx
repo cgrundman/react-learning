7e82d585e0a51ecc7329a0c22a9410a44870bb2f
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-07-07 11:27:07 +0200

    add debouncing for search

--- 25-patterns_and_best_practices/src/components/SearchableList/SearchableList.jsx
+++ 25-patterns_and_best_practices/src/components/SearchableList/SearchableList.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 export default function SearchableList({ items, itemKeyFn, children }) {
+    const lastChange = useRef()
     const [searchTerm, setSearchTerm] = useState('');
 
     const searchResults = items.filter((item) => 
@@ -8,7 +9,14 @@ export default function SearchableList({ items, itemKeyFn, children }) {
     );
 
     function handleChange(event) {
-        setSearchTerm(event.target.value);
+        if (lastChange.current) {
+            clearTimeout(lastChange.current)
+        }
+
+        lastChange.current = setTimeout(() => {
+            lastChange.current = null
+            setSearchTerm(event.target.value);
+        }, 1000);
     }
 
     return (
