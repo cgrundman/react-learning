2e5dbd2212aacdf7fc8b0a9252a0076395736f5f
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-07-07 11:21:32 +0200

    handle item keys dynamically

--- 25-patterns_and_best_practices/src/App.jsx
+++ 25-patterns_and_best_practices/src/App.jsx
@@ -72,10 +72,10 @@ function App() {
         </Accordion>
       </section>
       <section>
-        <SearchableList items={PLACES}>
+        <SearchableList items={PLACES} itemKeyFn={(item) => item.id}>
           {(item) => <Place item={item} />}
         </SearchableList>
-        <SearchableList items={['item 1', 'item 2']}>
+        <SearchableList items={['item 1', 'item 2']} itemKeyFn={(item) => item}>
           {(item) => item}
         </SearchableList>
       </section>
--- 25-patterns_and_best_practices/src/components/SearchableList/SearchableList.jsx
+++ 25-patterns_and_best_practices/src/components/SearchableList/SearchableList.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export default function SearchableList({ items, children }) {
+export default function SearchableList({ items, itemKeyFn, children }) {
     const [searchTerm, setSearchTerm] = useState('');
 
     const searchResults = items.filter((item) => 
@@ -15,8 +15,8 @@ export default function SearchableList({ items, children }) {
         <div className="searchable-list">
             <input type="search" placeholder="Search" onChange={handleChange}/>
             <ul>
-                {searchResults.map((item, index) => (
-                    <li key={index}>
+                {searchResults.map((item) => (
+                    <li key={itemKeyFn(item)}>
                         {children(item)}
                     </li>
                 ))}
