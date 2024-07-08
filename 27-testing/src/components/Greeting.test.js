3cd13356698f28ab7e77f357c4f4b37fb0e2d5f1
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-07-08 13:03:11 +0200

    grouping tests with the describe function

--- 27-testing/src/components/Greeting.test.js
+++ 27-testing/src/components/Greeting.test.js
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test('renders Hello World as a text', () => {
-    // Arrange
-    render(<Greeting />);
-
-    // Act
-    // ... nothing
-
-    // Assert
-    const helloWorldElement = screen.getByText('Hello World!')
-    expect(helloWorldElement).toBeInTheDocument();
-});
+describe('Greeting component', () => {
+    test('renders Hello World as a text', () => {
+        // Arrange
+        render(<Greeting />);
+    
+        // Act
+        // ... nothing
+    
+        // Assert
+        const helloWorldElement = screen.getByText('Hello World!')
+        expect(helloWorldElement).toBeInTheDocument();
+    });
+})
