9cd539a12ed813a312d181afa9dadaeef09557c6
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-07-08 15:25:49 +0200

    create a connected component

--- 27-testing/src/components/Greeting.js
+++ 27-testing/src/components/Greeting.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Output from "./Output";
 
 const Greeting = () => {
     const [changedText, setChangedText] = useState();
@@ -10,8 +11,8 @@ const Greeting = () => {
     return (
         <div>
             <h2>Hello World!</h2>
-            {!changedText && <p>It's good to see you!</p>}
-            {changedText && <p>Changed!</p>}
+            {!changedText && <Output>It's good to see you!</Output>}
+            {changedText && <Output>Changed!</Output>}
             <button onClick={changeTextHandler}>Change text!</button>
         </div>
     );
---
+++ 27-testing/src/components/Output.js
@@ -0,0 +1,7 @@
+const Output = props => {
+    return (
+        <p>{props.children}</p>
+    )
+};
+
+export default Output;
