451128e303caa9c5d220213ab0c27719f7e02515
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-07-09 21:16:17 +0200

    explore primitives

---
+++ 28-typescript/basic/basics.ts
@@ -0,0 +1,17 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number;
+
+age = 12.1;
+
+let userName: string;
+
+userName = 'Christian';
+
+let isStudent: boolean;
+
+isStudent = true;
