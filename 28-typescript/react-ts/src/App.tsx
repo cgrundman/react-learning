a297db6baf31b26237b2d44e9dbb451a68122ed4
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-07-12 14:28:27 +0200

    add data model for todo items

--- 28-typescript/react-ts/src/App.tsx
+++ 28-typescript/react-ts/src/App.tsx
@@ -1,9 +1,15 @@
 import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn Typescript')
+  ]
+
   return (
     <div>
-      <Todos items={['Learn Raect', 'Learn Typescript']}/>
+      <Todos items={todos}/>
     </div>
   );
 }
--- 28-typescript/react-ts/src/components/Todos.tsx
+++ 28-typescript/react-ts/src/components/Todos.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-const Todos: React.FC<{items: string[]}> = (props) => {
+import Todo from "../models/todo";
+
+const Todos: React.FC<{items: Todo[]}> = (props) => {
     return (
         <ul>
             {props.items.map(
-                item => <li key={item}>{item}</li>
+                item => <li key={item.id}>{item.text}</li>
             )}
         </ul>
     )
