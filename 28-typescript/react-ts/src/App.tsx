0b3ce5f634a52ecab654386222a1e1f369631933
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-07-12 15:35:48 +0200

    manage form and add function

--- 28-typescript/react-ts/src/App.tsx
+++ 28-typescript/react-ts/src/App.tsx
@@ -1,15 +1,18 @@
+import { useState } from 'react';
+
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn Typescript')
-  ]
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
 
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
   };
 
   return (
