2de89c97ba945f3d33621f6e5495da3d0e7115a3
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-07-12 16:10:42 +0200

    remove a todo item

--- 28-typescript/react-ts/src/App.tsx
+++ 28-typescript/react-ts/src/App.tsx
@@ -15,10 +15,16 @@ function App() {
     });
   };
 
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    })
+  };
+
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler}/>
-      <Todos items={todos}/>
+      <Todos items={todos} onRemoveTodo={removeTodoHandler}/>
     </div>
   );
 }
--- 28-typescript/react-ts/src/components/TodoItem.tsx
+++ 28-typescript/react-ts/src/components/TodoItem.tsx
@@ -1,8 +1,12 @@
 import classes from './TodoItem.module.css'
 
-const TodoItem: React.FC<{text: string}> = (props) => {
+const TodoItem: React.FC<{text: string; onRemoveTodo: () => void}> = (
+    props
+) => {
     return (
-        <li className={classes.item}>{props.text}</li>
+        <li onClick={props.onRemoveTodo} className={classes.item}>
+            {props.text}
+        </li>
     )
 }
 
